97f56187b43fb69fbf9429e838fe6537e177849a
Marco <51365505+MarcoMernberger@users.noreply.github.com>
2020-05-11 14:38:04 +0000

    added FromFile Annotator

--- src/mbf_genomics/genes/annotators.py
+++ src/mbf_genomics/genes/annotators.py
@@ -1,6 +1,8 @@
 from mbf_genomics.annotator import Annotator
+from pathlib import Path
+from typing import List
 import pandas as pd
-
+import numpy as np
 
 class Description(Annotator):
     """Add the description for the genes from genome.
@@ -29,3 +31,59 @@ class Description(Annotator):
         for gene_stable_id in ddf.df["gene_stable_id"]:
             result.append(lookup.get(gene_stable_id, ""))
         return pd.Series(result, index=ddf.df.index)
+
+
+class FromFile(Annotator):
+
+    def __init__(self, tablepath: Path, columns_to_add: List[str], index_column_table: str = "gene_stable_id", index_column_genes: str = "gene_stable_id", fill_value: float = None):
+        """
+        Adds arbitrary columns from a table.
+
+        This requires that both the table and the ddf have a common column on
+        which we can index.
+
+        Parameters
+        ----------
+        tablepath : Path
+            Path to table with additional columns.
+        columns_to_add : List[str]
+            List of columns to append.
+        index_column_table : str, optional
+            Index column in table, by default "gene_stable_id".
+        index_column_genes : str, optional
+            Index column in ddf to append to, by default "gene_stable_id".
+        fill_value : float, optonal
+            Value to fill for missing rows, defaults to np.NaN.
+        """
+        self.tablepath = tablepath
+        self.columns = columns_to_add
+        self.index_column_table = index_column_table
+        self.index_column_genes = index_column_genes
+        self.fill = fill_value if fill_value is not None else np.NaN
+
+    def parse(self):
+        if (self.tablepath.suffix == ".xls") or (self.tablepath.suffix == ".xlsx"):
+            return pd.read_excel(self.tablepath)
+        else:
+            return pd.read_csv(self.tablepath, sep="\t")
+
+    def get_cache_name(self):
+        return f"FromFile_{self.tablepath.name}"
+
+    def calc_ddf(self, ddf):
+        """Calculates the ddf to append."""
+        df_copy = ddf.df.copy()
+        if self.index_column_genes not in df_copy.columns:
+            raise ValueError(f"Column {self.index_column_genes} not found in ddf index, found was:\n{[str(x) for x in df_copy.columns]}.")
+        df_in = self.parse()
+        if self.index_column_table not in df_in.columns:
+            raise ValueError(f"Column {self.index_column_table} not found in table, found was:\n{[str(x) for x in df_in.columns]}.")
+        for column in self.columns:
+            if column not in df_in.columns:
+                raise ValueError(f"Column {column} not found in table, found was:\n{[str(x) for x in df_in.columns]}.")
+        df_copy.index = df_copy[self.index_column_genes]
+        df_in.index = df_in[self.index_column_table]
+        df_in = df_in.reindex(df_copy.index, fill_value=self.fill)
+        df_in = df_in[self.columns]
+        df_in.index = ddf.df.index
+        return df_in
--- tests/test_genes_annotators.py
+++ tests/test_genes_annotators.py
@@ -4,7 +4,7 @@ import pypipegraph as ppg
 from mbf_genomics import genes, DelayedDataFrame
 from mbf_genomics.testing import MockGenome
 from pypipegraph.testing import force_load
-
+from pathlib import Path
 
 @pytest.mark.usefixtures("new_pipegraph")
 class TestDescription:
@@ -72,3 +72,45 @@ class TestDescription:
         assert "ddf had no .genome and no genome was passed to Description" in str(
             g.anno_jobs[anno.get_cache_name()].lfg.exception
         )
+
+
+@pytest.mark.usefixtures("new_pipegraph")
+class TestFromFile:
+    def test_simple(self, tmpdir):
+        genome = MockGenome(
+            pd.DataFrame(
+                {
+                    "stable_id": ["a", "b", "c"],
+                    "chr": "1",
+                    "tss": [0, 100, 1000],
+                    "tes": [10, 101, 1010],
+                }
+            ),
+            df_genes_meta=pd.DataFrame(
+                {
+                    "gene_stable_id": ["a", "b", "c"],
+                    "description": ["hello", "world", "!"],
+                }
+            ).set_index("gene_stable_id"),
+        )
+        g = genes.Genes(genome)
+        df_to_add = pd.DataFrame({"testcol": [1, 2, 3], "index_vals": ["a", "b", "d"]}, index=["a", "b", "d"])
+        tmp_path = Path(tmpdir) / "dump.tsv"
+        df_to_add.to_csv(tmp_path, sep="\t", index=False)
+        anno = genes.annotators.FromFile(
+            tmp_path,
+            columns_to_add=["testcol"],
+            index_column_table="index_vals",
+            index_column_genes="gene_stable_id",
+            fill_value=-1
+        )
+        g += anno
+        force_load(g.annotate())
+        ppg.run_pipegraph()
+        print(g.df.index)
+        print(g.df)
+        assert "testcol" in g.df.columns
+        assert g.df.loc[0]["testcol"] == 1
+        assert g.df.loc[1]["testcol"] == 2
+        assert g.df.loc[2]["testcol"] == -1
+        assert len(g.df) == 3
