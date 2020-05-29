ad8de97ae91ee8009b23ae7b2420f0c1d5462c00
Marco <51365505+MarcoMernberger@users.noreply.github.com>
2020-05-29 15:27:29 +0200

    Added salmon quant annotator for genes

--- src/mbf_genomics/genes/annotators.py
+++ src/mbf_genomics/genes/annotators.py
@@ -1,4 +1,7 @@
 from mbf_genomics.annotator import Annotator
+from mbf_align.raw import Sample
+from mbf_genomes import EnsemblGenome
+from pypipegraph import Job
 import pandas as pd
 
 
@@ -29,3 +32,63 @@ class Description(Annotator):
         for gene_stable_id in ddf.df["gene_stable_id"]:
             result.append(lookup.get(gene_stable_id, ""))
         return pd.Series(result, index=ddf.df.index)
+
+
+class GeneStrandedSalmon(Annotator):
+    """
+    GeneStrandedSalmon is an Annotator to add Salmon counts to a ddf.
+
+    Adds two columns to the ddf containing the raw counts and TPMs
+    as privided by Salmon per lane.
+
+    Parameters
+    ----------
+    lane : Sample
+        Raw lane on which the Salmon call was performed.
+    salmon_job : Job
+        A job that generates the Salmon quant output from which the counts
+        are derived.
+    """
+    def __init__(self, lane: Sample, salmon_job: Job):
+        self.lane = lane
+        self.name = f"Raw count estimate {lane.name} (Salmon)"
+        self.salmon_output = f"results/Salmon/quant/{lane.name}/quant.genes.sf"
+        self.columns = [f"{lane.name} TPM (Salmon)", f"{lane.name} counts (Salmon)"]
+        self.cache_name = f"GeneStrandedSalmon_{lane.name}"
+        self.dependencies = [salmon_job]
+
+    def calc(self, df):
+        """
+        Calculates new columns for the DelayedDataFrame.
+
+        Adds a TPM column and a raw count column as counted by Salmon and
+        written to the the salmon output file.
+
+        Parameters
+        ----------
+        ddf : mbf_genomics.delayeddataframe.DelayedDataFrame
+            The ddf to add the columns to.
+
+        Returns
+        -------
+        DataFrame : pandas.DataFrame
+        Dataframe to add to the ddf.
+        """
+        df_salmon = pd.read_csv(self.salmon_output, sep="\t")
+        lookup = dict(zip(df["gene_stable_id"].values, df.index.values))
+        salmon_index = []
+        for stable_id in df_salmon["Name"]:
+            salmon_index.append(lookup[stable_id])
+        df_salmon.index = salmon_index
+        del df_salmon["Name"]
+        del df_salmon["Length"]
+        del df_salmon["EffectiveLength"]
+        df_salmon = df_salmon.rename(
+            columns={"TPM": self.columns[0], "NumReads": self.columns[1]}
+        )
+        df_salmon = df_salmon.reindex(labels=df.index).fillna(0)
+        return df_salmon
+
+    def deps(self, ddf):
+        """Return ppg.jobs"""
+        return self.dependencies
